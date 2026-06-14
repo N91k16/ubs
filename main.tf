a9cfffdcd978b2268c3fed7678929785d1e176db
nagendra <n91k16@gmail.com>
2026-06-14 18:04:12 +0530

    creating stg

--- main.tf
+++ main.tf
@@ -13,5 +13,12 @@ resource "azurerm_resource_group" "rgblock" {
   location = "eastus"
 }
 
+resource "azurerm_storage_account" "stblock" {
+  name                     = "stpune"
+  resource_group_name      = azurerm_resource_group.rgblock.name
+  location                 = azurerm_resource_group.rgblock.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+}
 
 
